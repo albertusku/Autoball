0d7e5d3ed36094427bff14754fecddf8ebe76184
Alberto_Ruiz <154091096+albertusku@users.noreply.github.com>
2025-09-17 13:22:11 +0200

    [VIDEOCAPTURE] Modifications to be able to check what is happening during a test

--- VideoCapture/VideoCapture.py
+++ VideoCapture/VideoCapture.py
@@ -13,6 +13,7 @@ MODEL_PATH = "../TrainModel/Model/Autoball_model.pth"
 IMAGE_SIZE = (224, 224)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 log= get_logger("VideoCapture")
+RTSP_URL = "rtsp://localhost:8554/mystream"
 
 
 if __name__ == "__main__":
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--framerate", type=int, default=30, help="Tasa de fotogramas por segundo (default: 30)")
     parser.add_argument("--video_file", type=str, default=video_path, help="Ruta al archivo de vídeo (opcional)")
     parser.add_argument("--images_per_sec", type=int, default=30, help="Imagenes por segundo enviadas al modelo (default: 10)")
+    parser.add_argument("--test",action="store_true",help="True si se ejecuta el test")
     args = parser.parse_args()
     frame_duration = 1.0 / args.framerate  # segundos por frame
 
@@ -39,6 +41,25 @@ if __name__ == "__main__":
     if capture.start():
         try:
             last_time = time.time()
+            first_frame = capture.read()
+            if first_frame is None:
+                raise RuntimeError("No se pudo capturar el primer frame para inicializar RTSP.")
+            height, width = first_frame.shape[:2]
+            ffmpeg_cmd = [
+                "ffmpeg",
+                "-re",
+                "-f", "rawvideo",
+                "-pix_fmt", "bgr24",
+                "-s", f"{width}x{height}",
+                "-r", str(fps),
+                "-i", "-",  # Entrada por stdin
+                "-c:v", "libx264",
+                "-preset", "ultrafast",
+                "-tune", "zerolatency",
+                "-f", "rtsp",
+                RTSP_URL
+                ]
+            proc = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
             while True:
                 current_time = time.time()
                 if current_time - last_time >= frame_duration:
@@ -49,17 +70,18 @@ if __name__ == "__main__":
                         with torch.no_grad():
                             output = model_config(input_tensor).squeeze().cpu().numpy()  
                         x_pred, y_pred = float(output[0]), float(output[1])
-                        height, width = frame.shape[:2]
 
                         # Convertir coordenadas normalizadas a píxeles
                         x_pixel = int(x_pred * width)
                         y_pixel = int(y_pred * height)
                         distance=capture.get_distance_to_middle(frame, x_pixel, y_pixel)
                         # Dibujar un círculo rojo (radio 8 px, grosor -1 = relleno)
-                        if args.source == "file":
-                            cv2.circle(frame, (x_pixel, y_pixel), 8, (0, 0, 255), -1)
-                            # Mostrar el frame con la predicción
-                            #cv2.imshow("Frame", frame)
+                        cv2.circle(frame, (x_pixel, y_pixel), 8, (0, 0, 255), -1)
+                        if args.test and proc is not None:
+                            proc.stdin.write(frame.tobytes())
+                        if args.source == "file" :
+                            cv2.imshow("Frame", frame)
+                            
                         last_time = time.time()
                 if cv2.waitKey(1) & 0xFF == ord('q') and args.source == "file":
                     break
@@ -68,5 +90,8 @@ if __name__ == "__main__":
         finally:
             capture.stop()
             cv2.destroyAllWindows()
+            if proc:
+                proc.stdin.close()
+                proc.wait()
     
     
